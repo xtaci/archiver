51384ad246c673db5cee4df9c8bdab583f51b946
xtaci <daniel820313@gmail.com>
2015-10-28 17:06:37 +0800

    update

--- src/replay/parser.go
+++ src/replay/parser.go
@@ -11,12 +11,31 @@ import (
 const (
 	TK_LS = iota
 	TK_SELECT
+	TK_REPLAY
+	TK_SHOW
+	TK_MGO
 )
 
 var cmds = map[string]int{
 	"ls":     TK_LS,
 	"select": TK_SELECT,
+	"replay": TK_REPLAY,
+	"show":   TK_SHOW,
+	"mgo":    TK_MGO,
 }
+
+var help = `REDO Replay Tool
+
+Commands:
+	ls 				-- list all database files
+	mgo mongodb://xxx/mydb		-- define mongodb url for replay
+
+	select xxx.RDO				-- choose a file
+		show(1, 100)			-- show all elements from 1, count 100
+		show(1234, 1,100)		-- show elements for a user(1234) from 1, count 100
+		replay(1234, 50, 100)		-- replay to user(1234) from 50 count 50
+		replay(1234)			-- replay all changes to a user(1234) 
+`
 
 type ToolBox struct {
 	dbs      map[string]*bolt.DB // all opened boltdb
